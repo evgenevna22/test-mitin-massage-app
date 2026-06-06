7a2530de35f9ec4e2ce9f3a35a6b119670506828
Tatiana Breneva <tbreneva@marfa-tech.com>
2026-06-06 15:55:28 +0200

    add site url

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,13 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    // Разрешаем ngrok-хост чтобы Vite отвечал на запросы через туннель
-    allowedHosts: ['ruby-daughter-antirust.ngrok-free.dev'],
-    headers: {
-      'ngrok-skip-browser-warning': 'true',
-      'user-agent': 'cussstom'
-    }
-  },
+  plugins: [vue()]
 })
